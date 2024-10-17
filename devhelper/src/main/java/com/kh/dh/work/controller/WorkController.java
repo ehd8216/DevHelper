46a83e3ec598a2e28aefdbe05fe85f10c18eb016
jte <wjdxordms123@naver.com>
2024-10-17 10:46:22 +0900

    스크랩 체크

--- devhelper/src/main/java/com/kh/dh/work/controller/WorkController.java
+++ devhelper/src/main/java/com/kh/dh/work/controller/WorkController.java
@@ -1,11 +1,13 @@
 package com.kh.dh.work.controller;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.dh.common.model.vo.PageInfo;
@@ -65,7 +67,17 @@ public class WorkController {
 		  .setViewName("work/scraplist");
 		return mv;
 	}
-	
+	@ResponseBody
+	@RequestMapping("scrapcheck.wo")
+	public List<Integer> scrapcheck(int memNo) {
+	    ArrayList<Scrap> scrap = wService.scraplist(memNo);
+	    List<Integer> SnList = new ArrayList<>();
+	    for (Scrap sc : scrap) {
+	        SnList.add(sc.getRecrutPblntSn()); // scrap 객체에서 recrutPblntSn을 추출
+	    }
+	    System.out.println(SnList); // 콘솔에 출력하여 확인
+	    return SnList; // SnList를 JSON 형태로 반환
+	}
 	
 	
 
